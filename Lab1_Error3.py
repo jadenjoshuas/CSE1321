52697ed46a6a33dcdc34712856e4856088b5d895
jadenjoshuas <128007847+jadenjoshuas@users.noreply.github.com>
2024-08-22 11:30:37 -0400

    Add files via upload

---
+++ Lab1_Error3.py
@@ -0,0 +1,15 @@
+# Program Lab1B.py
+# Demonstrate reading a string from the user.
+# Reads a character string from the user and prints it.
+
+# Class: CSE 1321L
+# Section:
+# Term: 1
+# Instructor: Tejaswini Karanam
+# Name: Jaden Joshua-Sunderraj
+# Lab: Intro to Python
+
+miles = input ("Enter the number of miles: ")
+gallons = input ("Enter the gallons of fuel used: ")
+mpg = int (miles) / float (gallons)
+print ("Miles Per Gallon: " + str (mpg))
