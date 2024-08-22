472b24b7b4325c0da738c09ed4bbb888e629bdae
jadenjoshuas <128007847+jadenjoshuas@users.noreply.github.com>
2024-08-22 11:29:21 -0400

    Add files via upload

---
+++ Lab1.py
@@ -0,0 +1,20 @@
+# Program Lab1A.py
+# Demonstrate the difference between print with and without a newline.
+# Prints two lines of output representing a rocket countdown.
+
+# Class: CSE 1321L
+# Section:
+# Term: 1
+# Instructor: Tejaswini Karanam
+# Name: Jaden Joshua-Sunderraj
+# Lab: Intro to Python
+
+print ("Three... ", end =" ")
+print ("Two... ", end =" ")
+print ("One... ", end =" ")
+print ("Zero..." , end =" ")
+print ("Liftoff!")
+# appears on first line
+print ("Houston, we have a problem.")
+
+
