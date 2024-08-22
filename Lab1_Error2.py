ec3a7897d230f0a54d05e036cfb9c86690e891ee
jadenjoshuas <128007847+jadenjoshuas@users.noreply.github.com>
2024-08-22 11:30:23 -0400

    Add files via upload

---
+++ Lab1_Error2.py
@@ -0,0 +1,13 @@
+# Program Lab1B.py
+# Demonstrate reading a string from the user.
+# Reads a character string from the user and prints it.
+
+# Class: CSE 1321L
+# Section:
+# Term: 1
+# Instructor: Tejaswini Karanam
+# Name: Jaden Joshua-Sunderraj
+# Lab: Intro to Python
+
+message = input ("Enter a line of text: ");
+print ("You wrote '" + message + "'");
